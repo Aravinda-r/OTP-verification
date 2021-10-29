89bf7680f0943cf990a7539c0e5eba2cb455eda8
Aravinda Reddy Pulagurla <85568574+Aravinda-r@users.noreply.github.com>
2021-10-29 21:59:45 +0530

    otp verification

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index";
 
 function App() {
+  const [otp, setOtp] = useState(new Array(6).fill(""));
+
+  const handleChange = (element, index) => {
+    if (isNaN(element.value)) return false;
+
+    setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
+
+    //Focus next input
+    if (element.nextSibling) {
+      element.nextSibling.focus();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="row">
+        <div className="col text-center">
+          <h2>Phone Verification</h2>
+          <hr />
+          <p>Enter the OTP sent to you to Mobile Number</p>
+
+          {otp.map((data, index) => {
+            return (
+              <input
+                className="otp-field"
+                type="text"
+                name="otp"
+                maxLength="1"
+                key={index}
+                value={data}
+                onChange={(e) => handleChange(e.target, index)}
+                onFocus={(e) => e.target.select()}
+              />
+            );
+          })}
+
+          <p>OTP Entered - {otp.join("")}</p>
+          <p>
+            <button
+              className="btn btn-secondary mr-2"
+              onClick={(e) => setOtp([...otp.map((v) => "")])}
+            >
+              Clear
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={(e) => alert("Entered OTP is " + otp.join(""))}
+            >
+              Verify OTP
+            </button>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
